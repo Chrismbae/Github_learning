170acbf4879cbd3f21ece4b3d4fa77a8d6f17633
Chrismbae <chrismbae@gmail.com>
2025-03-05 13:29:56 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt  # Fixed typo in import
+
 def calculate_mean(data):
     if not data:
         return 0
@@ -6,5 +9,20 @@ def calculate_mean(data):
 def calculate_variance(data):
     if not data:
         return 0
-    mean = calculate_mean(data)
+    mean = calculate_mean(data)  # Fixed typo: 'meaq' -> 'mean'
     return sum((x - mean) ** 2 for x in data) / len(data)
+
+def calculate_standard_deviation(data):
+    import math
+    return math.sqrt(calculate_variance(data))
+
+def calculate_median(data):
+    if not data:
+        return 0
+    data.sort()
+    n = len(data)
+    mid = n // 2
+    if n % 2 == 0:
+        return (data[mid - 1] + data[mid]) / 2
+    else:
+        return data[mid]
